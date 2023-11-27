8f35ebb08b7c7f340affcd6edb6561e4adf55886
JP Ungaretti <git@jungaretti.com>
2023-11-26 20:25:42 -0800

    Scaffold CLI app

--- Program.cs
+++ Program.cs
@@ -1,9 +1,33 @@
-﻿namespace advent_of_code_2023;
+using System.CommandLine;
+
+namespace advent_of_code_2023;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var dayArgument = new Argument<int>(
+            name: "day",
+            description: "The day to run."
+        );
+
+        var partArgument = new Argument<int>(
+            name: "part",
+            description: "The part to run."
+        );
+
+        var rootCommand = new RootCommand("Advent of Code 2023");
+        rootCommand.AddArgument(dayArgument);
+        rootCommand.AddArgument(partArgument);
+
+        rootCommand.SetHandler((day, part) =>
+            {
+                Console.WriteLine($"Day: {day}");
+                Console.WriteLine($"Part: {part}");
+            },
+            dayArgument,
+            partArgument);
+
+        return await rootCommand.InvokeAsync(args);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Advent of Code 2023 🎄
 
 My solutions for Advent of Code 2023.
+
+## Usage
+
+```bash
+dotnet restore
+```
