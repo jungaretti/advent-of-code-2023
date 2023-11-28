9506928f10d465cc5f36407c0ebd17a8d86533ec
JP Ungaretti <jpungaretti@microsoft.com>
2023-11-28 15:43:55 -0800

    Add test consts

--- src/AdventOfCode.Tests/PuzzleProviderTests.cs
+++ src/AdventOfCode.Tests/PuzzleProviderTests.cs
@@ -5,18 +5,22 @@ namespace AdventOfCode.Tests;
 public class PuzzleProviderTests
 {
     [Fact]
-    public void ProviderFindsInputAndSolver()
+    public void PuzzleSolverFindsStrategy()
     {
+        const int MOCK_DAY = 1;
+        const int MOCK_PART = 2;
+        const string MOCK_ANSWER = "success";
+
         var input = new string[] { "test" };
 
         var mockSolver = new Mock<IPuzzleStrategy>();
-        mockSolver.SetupGet(s => s.Day).Returns(1);
-        mockSolver.SetupGet(s => s.Part).Returns(1);
-        mockSolver.Setup(s => s.SolvePuzzle(input)).Returns("success");
+        mockSolver.SetupGet(s => s.Day).Returns(MOCK_DAY);
+        mockSolver.SetupGet(s => s.Part).Returns(MOCK_PART);
+        mockSolver.Setup(s => s.SolvePuzzle(input)).Returns(MOCK_ANSWER);
 
         var provider = new PuzzleSolver(new IPuzzleStrategy[] { mockSolver.Object });
 
-        var answer = provider.SolvePuzzle(1, 1, input);
-        Assert.Equal("success", answer);
+        var answer = provider.SolvePuzzle(MOCK_DAY, MOCK_PART, input);
+        Assert.Equal(MOCK_ANSWER, answer);
     }
 }
