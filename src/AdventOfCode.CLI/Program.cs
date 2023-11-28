141440024098b3b810feeadd7950b4c2f094ae43
JP Ungaretti <git@jungaretti.com>
2023-11-27 21:31:20 -0800

    Add CLI program

--- src/AdventOfCode.CLI/Program.cs
+++ src/AdventOfCode.CLI/Program.cs
@@ -1,9 +1,44 @@
+using System.CommandLine;
+
 namespace AdventOfCode.CLI;
 
 class Program
 {
-    static void Main(string[] args)
+    private static Action<int, int> rootCommandHandler = (day, part) => {
+        Console.WriteLine("Advent of Code 2023");
+        Console.WriteLine($"Solving day {day} part {part}");
+
+        // var solver = new PuzzleSolver(new PuzzleHandler[]
+        // {
+        //     // Add all puzzle handlers here
+        //     new Day00Part1(File.ReadAllLines("input/day00.txt")),
+        //     new Day00Part2(File.ReadAllLines("input/day00.txt")),
+        // });
+        // string answer = await solver.Solve(day, part);
+        var answer = "42";
+
+        Console.WriteLine();
+        Console.WriteLine($"Answer: {answer}");
+    };
+
+    static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var rootCommand = new RootCommand("Advent of Code 2023");
+
+        var dayArgument = new Argument<int>(
+            name: "day",
+            description: "The day to run."
+        );
+        rootCommand.AddArgument(dayArgument);
+
+        var partArgument = new Argument<int>(
+            name: "part",
+            description: "The part to run."
+        );
+        rootCommand.AddArgument(partArgument);
+
+        rootCommand.SetHandler(rootCommandHandler, dayArgument, partArgument);
+
+        return await rootCommand.InvokeAsync(args);
     }
 }
