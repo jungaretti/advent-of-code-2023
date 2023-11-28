7f3ac2ecc775d876b3a41340ff7e4130324c1978
JP Ungaretti <jpungaretti@microsoft.com>
2023-11-28 11:16:30 -0800

    Fix unit test

--- src/AdventOfCode.Tests/PuzzleProviderTests.cs
+++ src/AdventOfCode.Tests/PuzzleProviderTests.cs
@@ -5,16 +5,16 @@ public class PuzzleProviderTests
     [Fact]
     public async Task ProviderFindsInputAndSolver()
     {
-        var input = new PuzzleInput(1, new string[] { "1", "2", "3" });
+        var input = new string[] { "test" };
 
         var mockSolver = new Mock<PuzzleSolver>();
         mockSolver.SetupGet(s => s.Day).Returns(1);
         mockSolver.SetupGet(s => s.Part).Returns(1);
         mockSolver.Setup(s => s.SolveAsync(input)).ReturnsAsync("success");
 
-        var provider = new PuzzleProvider(new PuzzleInput[] { input }, new PuzzleSolver[] { mockSolver.Object });
+        var provider = new PuzzleProvider(new PuzzleSolver[] { mockSolver.Object });
 
-        var answer = await provider.SolveAsync(1, 1);
+        var answer = await provider.SolveAsync(1, 1, input);
         Assert.Equal("success", answer);
     }
 }
