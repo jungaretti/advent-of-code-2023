5a8687461bc1c01bc5389fea2cfcb943389a5258
JP Ungaretti <git@jungaretti.com>
2023-11-27 22:40:08 -0800

    Move root handler to lambda

--- src/AdventOfCode.CLI/Program.cs
+++ src/AdventOfCode.CLI/Program.cs
@@ -21,31 +21,29 @@ class Program
         );
         rootCommand.AddArgument(partArgument);
 
-        rootCommand.SetHandler(PuzzleHandler, dayArgument, partArgument);
+        rootCommand.SetHandler(async (day, part) =>
+        {
+            Console.WriteLine("Advent of Code 2023");
+            Console.WriteLine($"Solving day {day} part {part}");
+
+            var puzzleInputs = new PuzzleInput[]
+            {
+                new PuzzleInput(0, File.ReadAllLines("input/day00.txt")),
+            };
+
+            var puzzleSolvers = new PuzzleSolver[]
+            {
+                new Day00Part1(),
+                new Day00Part2(),
+            };
+
+            var provider = new PuzzleProvider(puzzleInputs, puzzleSolvers);
+            var answer = await provider.SolveAsync(day, part);
+
+            Console.WriteLine();
+            Console.WriteLine($"Answer: {answer}");
+        }, dayArgument, partArgument);
 
         return await rootCommand.InvokeAsync(args);
-    }
-
-    private static async Task PuzzleHandler(int day, int part)
-    {
-        Console.WriteLine("Advent of Code 2023");
-        Console.WriteLine($"Solving day {day} part {part}");
-
-        var puzzleInputs = new PuzzleInput[]
-        {
-            new PuzzleInput(0, File.ReadAllLines("input/day00.txt")),
-        };
-
-        var puzzleSolvers = new PuzzleSolver[]
-        {
-            new Day00Part1(),
-            new Day00Part2(),
-        };
-
-        var provider = new PuzzleProvider(puzzleInputs, puzzleSolvers);
-        var answer = await provider.SolveAsync(day, part);
-
-        Console.WriteLine();
-        Console.WriteLine($"Answer: {answer}");
     }
 }
