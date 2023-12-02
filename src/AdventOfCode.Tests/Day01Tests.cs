e498b46587a5958f90e6686e6c1f1cffe0ed58de
JP Ungaretti <git@jungaretti.com>
2023-12-02 14:10:32 -0800

    Move day class files

--- src/AdventOfCode.Tests/Day01Tests.cs
+++ src/AdventOfCode.Tests/Day01Tests.cs
@@ -2,7 +2,7 @@ using AdventOfCode;
 
 namespace AdventOfCode.Tests;
 
-public class DayTests
+public class Day01Tests
 {
     [Fact]
     public void Day01Part1Test()
@@ -37,22 +37,5 @@ public class DayTests
         var result = puzzleSolver.SolvePuzzle(1, 2, inputLines);
 
         Assert.Equal("281", result);
-    }
-
-    [Fact]
-    public void Day02Part1Test()
-    {
-        string[] inputLines = {
-            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-        };
-
-        var puzzleSolver = new PuzzleSolver();
-        var result = puzzleSolver.SolvePuzzle(2, 1, inputLines);
-
-        Assert.Equal("8", result);
     }
 }
