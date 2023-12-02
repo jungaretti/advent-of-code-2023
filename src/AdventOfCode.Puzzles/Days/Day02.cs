af548cf501c9dd28faee2336c77d238ca84661eb
JP Ungaretti <git@jungaretti.com>
2023-12-02 13:31:49 -0800

    Solve day 2 part 1

--- src/AdventOfCode.Puzzles/Days/Day02.cs
+++ src/AdventOfCode.Puzzles/Days/Day02.cs
@@ -1,12 +1,80 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Puzzles;
 
 class Day02 : IPuzzleDay
 {
+    private class GameResult
+    {
+        public int Id { get; }
+
+        public IEnumerable<IReadOnlyDictionary<string, int>> Rounds { get; }
+
+        public GameResult(string gameLine)
+        {
+            var gameRegex = new Regex(@"Game (\d+): (.+)");
+            var gameMatch = gameRegex.Match(gameLine);
+
+            string gameId = gameMatch.Groups[1].Value;
+            Id = int.Parse(gameId);
+
+            string gameText = gameMatch.Groups[2].Value;
+            Rounds = gameText.Split("; ").Select(roundText =>
+            {
+                var results = new Dictionary<string, int>();
+
+                foreach (var cubeText in roundText.Split(", "))
+                {
+                    var cubeRegex = new Regex(@"(\d+) (\w+)");
+                    var cubeMatch = cubeRegex.Match(cubeText);
+
+                    int cubeCount = int.Parse(cubeMatch.Groups[1].Value);
+                    string cubeColor = cubeMatch.Groups[2].Value;
+
+                    results.Add(cubeColor, cubeCount);
+                }
+
+                return results;
+            });
+        }
+
+        public bool IsPossible(IReadOnlyDictionary<string, int> allowedColorCounts)
+        {
+            foreach (var round in Rounds)
+            {
+                foreach (var color in round.Keys)
+                {
+                    if (!allowedColorCounts.ContainsKey(color))
+                    {
+                        return false;
+                    }
+
+                    if (round[color] > allowedColorCounts[color])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
     public int Day => 2;
 
     public string PartOne(IEnumerable<string> inputLines)
     {
-        throw new NotImplementedException();
+        IEnumerable<GameResult> results = inputLines.Select(line => new GameResult(line));
+
+        var allowedColorCounts = new Dictionary<string, int>
+        {
+            { "red", 12 },
+            { "green", 13 },
+            { "blue", 14 },
+        };
+        IEnumerable<GameResult> possibleResults = results.Where(result => result.IsPossible(allowedColorCounts));
+
+        return possibleResults.Select(result => result.Id).Sum().ToString();
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
--- src/AdventOfCode.Tests/DayTests.cs
+++ src/AdventOfCode.Tests/DayTests.cs
@@ -38,4 +38,21 @@ public class DayTests
 
         Assert.Equal("281", result);
     }
+
+    [Fact]
+    public void Day02Part1Test()
+    {
+        string[] inputLines = {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        };
+
+        var puzzleSolver = new PuzzleSolver();
+        var result = puzzleSolver.SolvePuzzle(2, 1, inputLines);
+
+        Assert.Equal("8", result);
+    }
 }
