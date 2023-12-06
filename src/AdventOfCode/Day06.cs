68a292524ffcacf5ce50f6fa9dd3ec2d0ca2ed07
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-06 14:55:13 -0800

    Parse races

--- src/AdventOfCode/Day06.cs
+++ src/AdventOfCode/Day06.cs
@@ -1,4 +1,6 @@
 
+using System.Data.SqlTypes;
+
 namespace AdventOfCode;
 
 class Day06 : IPuzzleDay
@@ -7,11 +9,40 @@ class Day06 : IPuzzleDay
 
     public string PartOne(IEnumerable<string> inputLines)
     {
-        throw new NotImplementedException();
+        IEnumerable<Race> races = Race.ParseRaces(inputLines);
+
+        var answer = races.Count();
+        return answer.ToString();
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
     {
         throw new NotImplementedException();
+    }
+
+    class Race
+    {
+        public int Time { get; }
+        public int Distance { get; }
+
+        public Race(int time, int distance)
+        {
+            Time = time;
+            Distance = distance;
+        }
+
+        static public IEnumerable<Race> ParseRaces(IEnumerable<string> inputLines)
+        {
+            const string TIME_PREFIX = "Time:";
+            const string DISTANCE_PREFIX = "Distance:";
+
+            string timeLine = inputLines.Single(line => line.StartsWith(TIME_PREFIX));
+            IEnumerable<string> times = timeLine.Substring(TIME_PREFIX.Length).Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string distanceLine = inputLines.Single(line => line.StartsWith(DISTANCE_PREFIX));
+            IEnumerable<string> distances = distanceLine.Substring(DISTANCE_PREFIX.Length).Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            return times.Zip(distances, (time, distance) => new Race(int.Parse(time), int.Parse(distance)));
+        }
     }
 }
