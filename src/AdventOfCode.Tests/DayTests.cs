cf53f523d11ee0a3f0b15de8f8ce35a0e4bcbdb5
JP Ungaretti <git@jungaretti.com>
2023-12-06 23:04:21 -0800

    Add test for day 7 part 2

--- src/AdventOfCode.Tests/DayTests.cs
+++ src/AdventOfCode.Tests/DayTests.cs
@@ -343,9 +343,14 @@ public class DayTests
     public void Day07Part2Test()
     {
         string[] inputLines = {
+            "32T3K 765",
+            "T55J5 684",
+            "KK677 28",
+            "KTJJT 220",
+            "QQQJA 483",
         };
 
         var answer = puzzleSolver.SolvePuzzle(7, 2, inputLines);
-        Assert.Equal("", answer);
+        Assert.Equal("5905", answer);
     }
 }
