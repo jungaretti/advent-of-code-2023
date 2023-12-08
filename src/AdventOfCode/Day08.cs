e812a070c1421c3ff02a3fb9760ffdbe69d62a1e
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-08 11:20:37 -0800

    Parse instructions

--- src/AdventOfCode/Day08.cs
+++ src/AdventOfCode/Day08.cs
@@ -1,4 +1,3 @@
-
 namespace AdventOfCode;
 
 class Day08 : IPuzzleDay
@@ -7,11 +6,31 @@ class Day08 : IPuzzleDay
 
     public string PartOne(IEnumerable<string> inputLines)
     {
+        var instructions = ParseInstructions(inputLines.First());
+
         throw new NotImplementedException();
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
     {
         throw new NotImplementedException();
+    }
+
+    private IEnumerable<Instruction> ParseInstructions(string instructionsLine)
+    {
+        return instructionsLine.Select(instruction => instruction switch
+        {
+            'L' => new Instruction(Direction.Left),
+            'R' => new Instruction(Direction.Right),
+            _ => throw new Exception($"Unknown instruction: {instruction}"),
+        });
+    }
+
+    private record Instruction(Direction Direction);
+
+    private enum Direction
+    {
+        Left,
+        Right,
     }
 }
