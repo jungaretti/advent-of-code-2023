614a55a44b194e862698d5580a40a18daf71a646
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-08 12:38:30 -0800

    Add test for day 8 part 2

--- src/AdventOfCode.Tests/DayTests.cs
+++ src/AdventOfCode.Tests/DayTests.cs
@@ -388,9 +388,19 @@ public class DayTests
     public void Day08Part2Test()
     {
         string[] inputLines = [
+            "LR",
+            "",
+            "11A = (11B, XXX)",
+            "11B = (XXX, 11Z)",
+            "11Z = (11B, XXX)",
+            "22A = (22B, XXX)",
+            "22B = (22C, 22C)",
+            "22C = (22Z, 22Z)",
+            "22Z = (22B, 22B)",
+            "XXX = (XXX, XXX)",
         ];
 
         var answer = puzzleSolver.SolvePuzzle(8, 2, inputLines);
-        Assert.Equal("", answer);
+        Assert.Equal("6", answer);
     }
 }
