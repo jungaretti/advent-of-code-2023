c03ccb3277741a70b6515a98e68be1e9835f8b9f
JP Ungaretti <git@jungaretti.com>
2023-12-09 09:38:09 -0800

    Solve day 9 part 1

--- src/AdventOfCode/Day09.cs
+++ src/AdventOfCode/Day09.cs
@@ -7,11 +7,43 @@ class Day09 : IPuzzleDay
 
     public string PartOne(IEnumerable<string> inputLines)
     {
-        return "0";
+        IEnumerable<History> histories = inputLines.Select(line => new History(line));
+        IEnumerable<int> nextValues = histories.Select(history => history.GetNextValue());
+
+        var answer = nextValues.Sum();
+        return answer.ToString();
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
     {
         return "0";
+    }
+
+    private class History
+    {
+        public IEnumerable<int> Values { get; }
+
+        public History(string historyLine)
+        {
+            Values = historyLine.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+        }
+
+        public int GetNextValue()
+        {
+            var nextValue = DeriveNextValue(Values);
+            return nextValue;
+        }
+
+        private int DeriveNextValue(IEnumerable<int> values)
+        {
+            if (values.All(value => value == 0))
+            {
+                return 0;
+            }
+
+            IEnumerable<int> derivatives = values.Zip(values.Skip(1), (last, current) => current - last);
+            int nextValue = values.Last() + DeriveNextValue(derivatives);
+            return nextValue;
+        }
     }
 }
