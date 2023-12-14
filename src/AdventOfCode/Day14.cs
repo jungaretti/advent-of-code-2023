2f9366d477a17863122b7566a49462a625444dec
JP Ungaretti <git@jungaretti.com>
2023-12-13 21:48:46 -0800

    Parse rocks

--- src/AdventOfCode/Day14.cs
+++ src/AdventOfCode/Day14.cs
@@ -1,4 +1,6 @@
 
+using System.Collections.Immutable;
+
 namespace AdventOfCode;
 
 class Day14 : IPuzzleDay
@@ -7,11 +9,32 @@ class Day14 : IPuzzleDay
 
     public string PartOne(IEnumerable<string> inputLines)
     {
-        throw new NotImplementedException();
+        var rocks = ParsePlatform(inputLines);
+        return "5";
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
     {
         throw new NotImplementedException();
+    }
+
+    private Platform ParsePlatform(IEnumerable<string> inputLines)
+    {
+        var rocks = inputLines.Select(line => line.Select<char, RockType?>(rockChar => rockChar switch
+        {
+            'O' => RockType.Rounded,
+            '#' => RockType.Cube,
+            '.' => null,
+            _ => throw new Exception($"Unknown rock type: {rockChar}")
+        }).ToArray()).ToArray();
+        return new Platform(rocks);
+    }
+
+    record Platform(RockType?[][] Rocks);
+
+    enum RockType
+    {
+        Rounded,
+        Cube,
     }
 }
