78c9ffc03a886806b250cb6dbce1b38b25419f2d
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-19 12:39:20 -0600

    Add more tests

--- src/AdventOfCode.Tests/DayTests.cs
+++ src/AdventOfCode.Tests/DayTests.cs
@@ -495,6 +495,63 @@ public class DayTests
             "gd{a>3333:R,R}",
             "hdj{m>838:A,pv}",
             "",
+            "{x=787,m=2655,a=1222,s=2876}"
+        ];
+
+        var answer = puzzleSolver.SolvePuzzle(19, 1, inputLines);
+        Assert.Equal("7540", answer);
+
+        inputLines = [
+            "px{a<2006:qkq,m>2090:A,rfg}",
+            "pv{a>1716:R,A}",
+            "lnx{m>1548:A,A}",
+            "rfg{s<537:gd,x>2440:R,A}",
+            "qs{s>3448:A,lnx}",
+            "qkq{x<1416:A,crn}",
+            "crn{x>2662:A,R}",
+            "in{s<1351:px,qqz}",
+            "qqz{s>2770:qs,m<1801:hdj,R}",
+            "gd{a>3333:R,R}",
+            "hdj{m>838:A,pv}",
+            "",
+            "{x=2036,m=264,a=79,s=2244}",
+        ];
+
+        answer = puzzleSolver.SolvePuzzle(19, 1, inputLines);
+        Assert.Equal("4623", answer);
+
+        inputLines = [
+            "px{a<2006:qkq,m>2090:A,rfg}",
+            "pv{a>1716:R,A}",
+            "lnx{m>1548:A,A}",
+            "rfg{s<537:gd,x>2440:R,A}",
+            "qs{s>3448:A,lnx}",
+            "qkq{x<1416:A,crn}",
+            "crn{x>2662:A,R}",
+            "in{s<1351:px,qqz}",
+            "qqz{s>2770:qs,m<1801:hdj,R}",
+            "gd{a>3333:R,R}",
+            "hdj{m>838:A,pv}",
+            "",
+            "{x=2127,m=1623,a=2188,s=1013}",
+        ];
+
+        answer = puzzleSolver.SolvePuzzle(19, 1, inputLines);
+        Assert.Equal("6951", answer);
+
+        inputLines = [
+            "px{a<2006:qkq,m>2090:A,rfg}",
+            "pv{a>1716:R,A}",
+            "lnx{m>1548:A,A}",
+            "rfg{s<537:gd,x>2440:R,A}",
+            "qs{s>3448:A,lnx}",
+            "qkq{x<1416:A,crn}",
+            "crn{x>2662:A,R}",
+            "in{s<1351:px,qqz}",
+            "qqz{s>2770:qs,m<1801:hdj,R}",
+            "gd{a>3333:R,R}",
+            "hdj{m>838:A,pv}",
+            "",
             "{x=787,m=2655,a=1222,s=2876}",
             "{x=1679,m=44,a=2067,s=496}",
             "{x=2036,m=264,a=79,s=2244}",
@@ -502,7 +559,7 @@ public class DayTests
             "{x=2127,m=1623,a=2188,s=1013}",
         ];
 
-        var answer = puzzleSolver.SolvePuzzle(19, 1, inputLines);
+        answer = puzzleSolver.SolvePuzzle(19, 1, inputLines);
         Assert.Equal("19114", answer);
     }
 }
