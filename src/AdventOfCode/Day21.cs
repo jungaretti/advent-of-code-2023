31b6a75742cd606d294174b1df2848d8fa97e205
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-21 10:58:49 -0600

    Basic walking algorithm

--- src/AdventOfCode/Day21.cs
+++ src/AdventOfCode/Day21.cs
@@ -1,3 +1,4 @@
+using System.Text;
 
 namespace AdventOfCode;
 
@@ -8,7 +9,10 @@ class Day21 : IPuzzleDay
     public string PartOne(IEnumerable<string> inputLines)
     {
         var garden = new Garden(inputLines);
-        return "0";
+        var possibleTiles = garden.Walk(6);
+
+        var answer = possibleTiles.Count();
+        return answer.ToString();
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
@@ -27,7 +31,42 @@ class Day21 : IPuzzleDay
                 return new Tile(column, rowIndex, columnIndex);
             }).ToArray()).ToArray();
         }
+
+        public IEnumerable<Tile> Walk(int steps)
+        {
+            Direction north = new(-1, 0);
+            Direction south = new(1, 0);
+            Direction east = new(0, 1);
+            Direction west = new(0, -1);
+            List<Direction> possibleDirections = [north, south, east, west];
+
+            Tile startTile = tiles
+                .SelectMany(row => row)
+                .Single(tile => tile.Type == 'S');
+
+            HashSet<Tile> currentTiles = [startTile];
+            for (int stepCount = 0; stepCount < steps; stepCount++)
+            {
+                HashSet<Tile> nextTiles = new();
+                foreach (Tile tile in currentTiles)
+                {
+                    foreach (Direction direction in possibleDirections)
+                    {
+                        Tile nextTile = tiles[tile.Row + direction.RowDiff][tile.Col + direction.ColDiff];
+                        if (nextTile.Type == '.' || nextTile.Type == 'S')
+                        {
+                            nextTiles.Add(nextTile);
+                        }
+                    }
+                }
+                currentTiles = nextTiles;
+            }
+
+            return currentTiles;
+        }
     }
 
     private record Tile(char Type, int Row, int Col);
+
+    private record Direction(int RowDiff, int ColDiff);
 }
