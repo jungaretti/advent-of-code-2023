8ae1bb46d977b7b6c0e4217a56db0e3fc67db82d
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-22 13:18:43 -0600

    Pass test

--- src/AdventOfCode/Day22.cs
+++ src/AdventOfCode/Day22.cs
@@ -10,8 +10,25 @@ class Day22 : IPuzzleDay
     public string PartOne(IEnumerable<string> inputLines)
     {
         var bricks = ParseBricks(inputLines);
+        var settledBricks = SettleBricks(bricks);
 
-        return "0";
+        var safeToRemoveBricks = settledBricks.Where(removedBrick =>
+        {
+            // Try to settle the bricks without this brick
+            var newlySettledBricks = SettleBricks(settledBricks.Except(new[] { removedBrick }));
+
+            // If removing this brick causes other bricks to move, it is not safe to remove
+            if (newlySettledBricks.Except(settledBricks).Any(brick => brick != removedBrick))
+            {
+                return false;
+            }
+
+            // If removing this brick does not change the settled bricks, then we can remove it
+            return true;
+        });
+
+        var answer = safeToRemoveBricks.Count();
+        return answer.ToString();
     }
 
     public string PartTwo(IEnumerable<string> inputLines)
@@ -54,6 +71,35 @@ class Day22 : IPuzzleDay
         }
     }
 
+    private List<Brick> SettleBricks(IEnumerable<Brick> bricks)
+    {
+        var allOrderedBricks = bricks.OrderBy(brick => brick.Start.Z);
+
+        List<Brick> allSettledBricks = new();
+        foreach (Brick brick in allOrderedBricks)
+        {
+            IEnumerable<Coordinate> allSettledCoordinates = allSettledBricks.SelectMany(GetAllCoordinates);
+
+            var settledBrick = brick;
+            var shiftedBrick = ShiftBrick(settledBrick, z: -1);
+            while (GetAllCoordinates(shiftedBrick).Intersect(allSettledCoordinates).Any() == false)
+            {
+                // Do not allow bricks to go below Z=0
+                if (shiftedBrick.Start.Z <= 0 || shiftedBrick.End.Z <= 0)
+                {
+                    break;
+                }
+
+                // Move the brick down one layer
+                settledBrick = shiftedBrick;
+                shiftedBrick = ShiftBrick(shiftedBrick, z: -1);
+            }
+
+            allSettledBricks.Add(settledBrick);
+        }
+        return allSettledBricks;
+    }
+
     private IEnumerable<Coordinate> GetAllCoordinates(Brick brick)
     {
         for (int x = brick.Start.X; x <= brick.End.X; x++)
@@ -66,6 +112,25 @@ class Day22 : IPuzzleDay
                 }
             }
         }
+    }
+
+    private Brick ShiftBrick(Brick brick, int x = 0, int y = 0, int z = 0)
+    {
+        return brick with
+        {
+            Start = brick.Start with
+            {
+                X = brick.Start.X + x,
+                Y = brick.Start.Y + y,
+                Z = brick.Start.Z + z
+            },
+            End = brick.End with
+            {
+                X = brick.End.X + x,
+                Y = brick.End.Y + y,
+                Z = brick.End.Z + z
+            }
+        };
     }
 
     private record Brick(Coordinate Start, Coordinate End);
