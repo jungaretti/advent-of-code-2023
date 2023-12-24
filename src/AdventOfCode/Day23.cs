da2d0a8046b3c71e39ad280dca72f358f738c113
JP Ungaretti <jpungaretti@microsoft.com>
2023-12-24 13:22:38 -0600

    Solve day 23 part 2

--- src/AdventOfCode/Day23.cs
+++ src/AdventOfCode/Day23.cs
@@ -82,9 +82,7 @@ class Day23 : IPuzzleDay
             }
         }
 
-        // Find the longest path
         var dfsSeen = new HashSet<Coordinate>();
-
         int LongestDistance(Coordinate coordinate)
         {
             if (coordinate == end)
@@ -108,7 +106,97 @@ class Day23 : IPuzzleDay
 
     public string PartTwo(IEnumerable<string> inputLines)
     {
-        return "0";
+        char[][] grid = inputLines.Select(s => s.ToCharArray()).ToArray();
+
+        Coordinate start = new(0, grid.First().TakeWhile(c => c != '.').Count());
+        Coordinate end = new(grid.Length - 1, grid.Last().TakeWhile(c => c != '.').Count());
+
+        // Find "interesting" points where we can make a decision
+        // Trim walled points to reduce the number of points to consider
+        IEnumerable<Coordinate> points = grid
+            .SelectMany((row, rowIndex) => row.Select((value, colIndex) =>
+            {
+                if (value == '#')
+                    return null;
+
+                var neighbors = new Coordinate[] {
+                    new(rowIndex - 1, colIndex),
+                    new(rowIndex + 1, colIndex),
+                    new(rowIndex, colIndex - 1),
+                    new(rowIndex, colIndex + 1),
+                };
+                var validNeighbors = neighbors
+                    .Where(c => c.Row >= 0 && c.Row < grid.Length)
+                    .Where(c => c.Column >= 0 && c.Column < row.Length)
+                    .Where(c => grid[c.Row][c.Column] != '#');
+
+                if (validNeighbors.Count() >= 3)
+                    return new Coordinate(rowIndex, colIndex);
+                else
+                    return null;
+            }))
+            .OfType<Coordinate>()
+            .Append(start)
+            .Append(end);
+
+        // Build a graph of all the points and their distances
+        // We'll use this graph to find the longest path
+        Dictionary<Coordinate, Dictionary<Coordinate, int>> graph = points
+            .ToDictionary(p => p, p => new Dictionary<Coordinate, int>());
+        foreach (var startingPoint in points)
+        {
+            var stack = new Stack<(Coordinate coordinate, int distance)>([(startingPoint, 0)]);
+            var seen = new HashSet<Coordinate>([startingPoint]);
+
+            while (stack.TryPop(out var point))
+            {
+                if (point.distance > 0 && points.Contains(point.coordinate))
+                {
+                    graph[startingPoint][point.coordinate] = point.distance;
+                    continue;
+                }
+
+                foreach (var direction in new List<Coordinate>() { new(-1, 0), new(1, 0), new(0, -1), new(0, 1) })
+                {
+                    var newCoordinate = new Coordinate(point.coordinate.Row + direction.Row, point.coordinate.Column + direction.Column);
+
+                    if (
+                        newCoordinate.Row >= 0 && newCoordinate.Row < grid.Length &&
+                        newCoordinate.Column >= 0 && newCoordinate.Column < grid[newCoordinate.Row].Length &&
+                        grid[newCoordinate.Row][newCoordinate.Column] != '#' &&
+                        !seen.Contains(newCoordinate)
+                    )
+                    {
+                        stack.Push((newCoordinate, point.distance + 1));
+                        seen.Add(newCoordinate);
+                    }
+                }
+            }
+        }
+
+        var dfsSeen = new HashSet<Coordinate>();
+        int LongestDistance(Coordinate coordinate)
+        {
+            if (coordinate == end)
+                return 0;
+
+            var m = int.MinValue;
+
+            dfsSeen.Add(coordinate);
+            foreach (var nextCoordinate in graph[coordinate].Keys)
+            {
+                if (dfsSeen.Contains(nextCoordinate))
+                    continue;
+
+                m = Math.Max(m, LongestDistance(nextCoordinate) + graph[coordinate][nextCoordinate]);
+            }
+            dfsSeen.Remove(coordinate);
+
+            return m;
+        }
+
+        var answer = LongestDistance(start);
+        return answer.ToString();
     }
 
     private record Coordinate(int Row, int Column);
